6adac2f4611f0dc025822aca7910a182ae61e45f
GreatnessGrace <graceunstoppable7@gmail.com>
2025-07-16 16:38:22 +0530

    Initialize database and message queue connections, and set up Redis client

--- backend/user-service/src/index.ts
+++ backend/user-service/src/index.ts
@@ -1,8 +1,24 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import cors from 'cors';
+import { createClient } from 'redis';
+import { connectRabbitMQ } from './config/rabbitmq';
+import connectDB from './config/db';
 
 dotenv.config();
+
+connectDB();
+
+connectRabbitMQ();
+
+export const redisClient = createClient({
+    url: process.env.REDIS_URL,
+});
+
+redisClient
+    .connect()
+    .then(() => console.log("Connected to Redis"))
+    .catch(console.error);
 
 const app = express();
 const PORT = process.env.PORT || 5000;
