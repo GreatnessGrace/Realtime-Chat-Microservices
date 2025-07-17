1da13259731e6df3856ee8d4d5bb653153332e80
GreatnessGrace <graceunstoppable7@gmail.com>
2025-07-17 12:11:58 +0530

    API: sendMessage

--- backend/chat-service/src/routes/chat.ts
+++ backend/chat-service/src/routes/chat.ts
@@ -3,6 +3,7 @@ import isAuth from "../middlewares/isAuth.js";
 import {
   createNewChat,
   getAllChats,
+  sendMessage,
 } from "../controllers/chat.js";
 import { upload } from "../middlewares/multer.js";
 
@@ -10,5 +11,6 @@ const router = express.Router();
 
 router.post("/chat/new", isAuth, createNewChat);
 router.get("/chat/all", isAuth, getAllChats);
+router.post("/message", isAuth, upload.single("image"), sendMessage);
 
 export default router;
