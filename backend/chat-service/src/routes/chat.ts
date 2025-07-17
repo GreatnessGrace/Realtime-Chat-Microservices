b0bbcc7e6322407bd1d28527b7bba0647f4a86c8
GreatnessGrace <graceunstoppable7@gmail.com>
2025-07-17 12:16:17 +0530

    API: getMessagesByChat

--- backend/chat-service/src/routes/chat.ts
+++ backend/chat-service/src/routes/chat.ts
@@ -4,6 +4,7 @@ import {
   createNewChat,
   getAllChats,
   sendMessage,
+  getMessagesByChat
 } from "../controllers/chat.js";
 import { upload } from "../middlewares/multer.js";
 
@@ -12,5 +13,6 @@ const router = express.Router();
 router.post("/chat/new", isAuth, createNewChat);
 router.get("/chat/all", isAuth, getAllChats);
 router.post("/message", isAuth, upload.single("image"), sendMessage);
+router.get("/message/:chatId", isAuth, getMessagesByChat);
 
 export default router;
