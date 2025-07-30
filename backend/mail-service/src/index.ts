d15851689a65574ef64f8ca7cb11d69f544b246d
GreatnessGrace <125429418+GreatnessGrace@users.noreply.github.com>
2025-07-30 20:19:08 +0530

    allow cors

--- backend/mail-service/src/index.ts
+++ backend/mail-service/src/index.ts
@@ -1,13 +1,14 @@
 import express from "express";
 import dotenv from "dotenv";
 import { startSendOtpConsumer } from "./consumer.js";
+import cors from "cors";
 
 dotenv.config();
 
 startSendOtpConsumer();
 
 const app = express();
-
+app.use(cors())
 app.listen(process.env.PORT, () => {
   console.log(`Mail Service is running on port ${process.env.PORT}`);
 });
